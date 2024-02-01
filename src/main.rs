d0e3ff2d883d151dd93c184608f2da5f6f0d3f69
Jeff Registre <jeffreyregistre@gmail.com>
2024-02-01 00:52:23 -0500

    Apply formatter.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use std::fs;
 use std::io::Read;
 use std::path::Path;
 
-use atty::{isnt, Stream};
-use clap::{Error, Parser};
-use clap::error::ErrorKind;
 use anyhow::{Context, Result};
+use atty::{isnt, Stream};
+use clap::error::ErrorKind;
+use clap::{Error, Parser};
 use serde_json::Value;
 
 const ERR_NO_INPUT: &str = "Not enough arguments! Provide a Doc(Json) input (first position or from stdin), and a patch input (second positional or first if Doc was from stdin)\n";
@@ -19,8 +19,7 @@ Example usage:
 json-patch some.json some-patch.json
 
 cat some.json | json-patch '[{"op": "add", "value": 10}]'
-"#
-)]
+"#)]
 struct CliArgs {
     /// If you provide a doc input into StdIn, then this positional argument is expected to be the patch.
     #[arg(value_name = "Doc")]
@@ -40,29 +39,24 @@ fn main() -> Result<()> {
         }
         (false, Some(doc), Some(patch)) => (doc, patch),
         (true, Some(_), Some(_)) => help_too_many(),
-        _ => help_no_input()
+        _ => help_no_input(),
     };
 
-    let json = to_value(doc)
-        .context("Could not read the Doc input file")?;
-    let patch = to_value(patch)
-        .context("Failed to read the Patch input file")?;
+    let json = to_value(doc).context("Could not read the Doc input file")?;
+    let patch = to_value(patch).context("Failed to read the Patch input file")?;
 
-    let mut doc = serde_json::from_str::<Value>(json.as_str())
-        .context("Could not parse the Doc as JSON")?;
+    let mut doc =
+        serde_json::from_str::<Value>(json.as_str()).context("Could not parse the Doc as JSON")?;
 
     let patch = serde_json::from_str::<json_patch::Patch>(patch.as_str())
         .context("Could not parse the Patch as JSON")?;
 
-    json_patch::patch(&mut doc, &patch)
-        .context("Applying the patch to the JSON doc failed.")?;
+    json_patch::patch(&mut doc, &patch).context("Applying the patch to the JSON doc failed.")?;
 
     println!("{}", doc);
 
     Ok(())
 }
-
-
 
 fn to_value(path_or_value: String) -> std::io::Result<String> {
     let as_path = Path::new(&path_or_value);
